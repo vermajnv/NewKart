0998aa1a981031447ffa31a498a700228496a727
Nayan Verma <nayan@hipster-inc.com>
2019-02-19 00:42:08 +0530

    Getting data through database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/Product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Shopping Kart new' });
+   Product.find(function(err, docs) {
+      res.render('shop/index', {
+         title: 'Shopping Kart new',
+         products : docs
+      });
+   });
+
 });
 
 module.exports = router;
