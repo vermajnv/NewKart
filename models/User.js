e136c1bb3d2f3aab8f25cae32bebfd710fa1e316
Nayan Verma <nayan@hipster-inc.com>
2019-02-19 23:33:00 +0530

    User model to handle registration request

---
+++ models/User.js
@@ -0,0 +1,9 @@
+let mongoose = require('mongoose');
+let schema = mongoose.Schema;
+
+let userSchema = new Schema({
+   email : {type : String, required : true},
+   password : { type : String, required : true}
+});
+
+module.exports = mongoose.model('User', userSchema);
