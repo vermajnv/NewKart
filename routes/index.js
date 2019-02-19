63139ea7be9571b1a1b397a6900430553796228b
Nayan Verma <nayan@hipster-inc.com>
2019-02-19 23:33:58 +0530

    adding user signup form with csrf protection

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,31 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/Product');
+var csrf = require('csurf');
 
+let csrfProtection = csrf();
+
+router.use(csrfProtection);
 /* GET home page. */
 router.get('/', function(req, res, next) {
    Product.find(function(err, docs) {
       res.render('shop/index', {
          title: 'Shopping Kart new',
-         products : docs
+         products: docs
       });
    });
+});
 
+// GET sign up form
+router.get('/user/signup', function(req, res, next) {
+   res.render('user/signup', {
+      csrfToken: req.csrfToken()
+   });
+});
+
+// Store user
+router.post('/user/signup', function(req, res, next) {
+   res.redirect('/');
 });
 
 module.exports = router;
